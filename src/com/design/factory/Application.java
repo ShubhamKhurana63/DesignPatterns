78a0d8b3a0eded34d28ab53849f8c8d541e9399d
shubham khurana <shubhamkhurana63@gmail.com>
2017-10-01 22:50:23 +0530

    committing dummy

--- src/com/design/factory/Application.java
+++ src/com/design/factory/Application.java
@@ -4,6 +4,9 @@ public class Application {
 
 	public static void main(String[] args) {
 
+	System.out.println("we are in the develop branch of the code");
+	
+	
 		Bank bank = Factory.getBankInstance("HDFC");
 		bank.bankIntro();
 
